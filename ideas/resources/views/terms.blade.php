87c2f6553c53bb2de26cd0a780641f223c8a78c0
lucas-catto <lucascattosilva2024@gmail.com>
2024-11-27 22:45:25 -0300

    Blade templates & Layouts | Laravel 10 Tutorial #7

---
+++ ideas/resources/views/terms.blade.php
@@ -0,0 +1,9 @@
+
+@extends('layout/layout')
+
+@section('content')
+    <h1>Terms</h1>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt culpa ratione ea molestias dolorum et optio nemo reprehenderit aliquid! Laboriosam provident sint modi. Magni voluptatibus nesciunt doloribus quo, similique cum.
+    </div>
+@endsection
--- ideas/routes/web.php
+++ ideas/routes/web.php
@@ -6,4 +6,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 
 Route::get('', [DashboardController::class, 'index']);
-Route::get('profile', [ProfileController::class, 'index']);
+
+Route::get('terms', function() {
+    return view('terms');
+});
