a144538dfc6dec9dbc4033aa1f106d7099b160e6
lucas-catto <lucascattosilva2024@gmail.com>
2024-11-30 21:52:17 -0300

    Laravel Form submission | Laravel 10 Tutorial #10

--- ideas/routes/web.php
+++ ideas/routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\ProfileController;
 
-Route::get('', [DashboardController::class, 'index']);
+Route::get('', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::post('idea', [IdeaController::class, 'store'])->name('idea.store');
 
 Route::get('terms', function() {
     return view('terms');
